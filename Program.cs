09078b721a2c7d6d6cfd72a704e01120dc581520
Alex Vlasov <drumlife182@gmail.com>
2023-02-08 14:01:17 +0100

    Initial project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace http
+{
+  class Program
+  {
+    HttpClient client = new HttpClient();
+    static async Task Main(string[] args)
+    {
+      Program program = new Program();
+      await program.GetTodoItems();
+    }
+
+    private async Task GetTodoItems()
+    {
+      string response = await client.GetStringAsync("https://api.publicapis.org/entries");
+      Console.WriteLine(response);
+    }
+  }
+}
